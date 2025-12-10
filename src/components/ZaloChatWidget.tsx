dc29a8373273735d24de5eaa191de18f7aa2bf67
zhouxianping1 <390447296@qq.com>
2025-12-10 17:18:54 +0700

    描述本次修改

--- src/components/ZaloChatWidget.tsx
+++ src/components/ZaloChatWidget.tsx
@@ -19,10 +19,11 @@ export default function ZaloChatWidget() {
 
   return (
     // 现在 Zalo 聊天窗口的位置是由外层这个 div 容器控制的
-    // 使用 Tailwind 的 fixed bottom-6 right-6 将整个区块固定在右下角
+    // 使用响应式 Tailwind 类：移动端 bottom-4 right-4 (16px)，桌面端 bottom-6 right-6 (24px)
+    // 与电话按钮对齐：移动端 16px，桌面端 24px
     // z-[9999] 确保浮层在最上层，同时允许聊天窗口正常弹出
     // 外层容器不限制 overflow，让 SDK 创建的聊天窗口可以完整显示，不会被裁剪
-    <div className="fixed bottom-6 right-6 z-[9999]">
+    <div className="fixed bottom-4 right-4 sm:bottom-6 sm:right-6 z-[9999]">
       {/* 里面这个才是 Zalo 官方需要的挂载点，样式交给 SDK 自己控制 */}
       {/* 不添加任何强制样式覆盖，让 SDK 完全控制聊天窗口的位置和大小 */}
       {/* 为什么不会再被裁剪：
